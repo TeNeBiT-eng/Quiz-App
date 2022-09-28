de20ea863ef8629c387ca69ac0d672f1ed27d618
unknown <ademolababalola2003@gmail.com>
2022-09-28 09:03:07 +0100

    set questions and answers

--- src/App.jsx
+++ src/App.jsx
@@ -6,75 +6,114 @@ function App() {
     {
       questionText: "What is 2 + 2",
       answerOptions: [
-        {answerText: "6", isCorrect: false},
-        {answerText: "8", isCorrect: false},
-        {answerText: "22", isCorrect: false},
-        {answerText: "4", isCorrect: true},
-      ]
+        { answerText: "6", isCorrect: false },
+        { answerText: "8", isCorrect: false },
+        { answerText: "22", isCorrect: false },
+        { answerText: "4", isCorrect: true },
+      ],
     },
 
     {
       questionText: "What is 2 - 2",
       answerOptions: [
-        {answerText: "9", isCorrect: false},
-        {answerText: "0", isCorrect: true},
-        {answerText: "2", isCorrect: false},
-        {answerText: "1", isCorrect: false},
-      ]
+        { answerText: "9", isCorrect: false },
+        { answerText: "0", isCorrect: true },
+        { answerText: "2", isCorrect: false },
+        { answerText: "1", isCorrect: false },
+      ],
     },
 
     {
       questionText: "What is 3 x 2",
       answerOptions: [
-        {answerText: "6", isCorrect: true},
-        {answerText: "8", isCorrect: false},
-        {answerText: "9", isCorrect: false},
-        {answerText: "10", isCorrect: false},
-      ]
+        { answerText: "6", isCorrect: true },
+        { answerText: "8", isCorrect: false },
+        { answerText: "9", isCorrect: false },
+        { answerText: "10", isCorrect: false },
+      ],
     },
 
     {
       questionText: "What is 9/3",
       answerOptions: [
-        {answerText: "3", isCorrect: true},
-        {answerText: "8", isCorrect: false},
-        {answerText: "2", isCorrect: false},
-        {answerText: "4", isCorrect: false},
-      ]
+        { answerText: "3", isCorrect: true },
+        { answerText: "8", isCorrect: false },
+        { answerText: "2", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+      ],
     },
 
     {
       questionText: "What is 4 + 3",
       answerOptions: [
-        {answerText: "6", isCorrect: false},
-        {answerText: "7", isCorrect: true},
-        {answerText: "11", isCorrect: false},
-        {answerText: "9", isCorrect: false},
-      ]
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+        { answerText: "11", isCorrect: false },
+        { answerText: "9", isCorrect: false },
+      ],
     },
-  ]
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  // const nxtQuestion = currentQuestion + 1;
+  // const prvQuestion = currentQuestion + 1;
+
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+
+  const answerBtnClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <>
       <section>
-        <div className="card">
-          <div className="question">
-            <div className="questionCount">
-              <p>
-                <span>Question 1</span> / 5
-              </p>
-            </div>
-            <div className="questionText">
-              <p>Question goes here</p>
-            </div>
+        {/* <p className="NB">
+          <span className="note">NOTE TO SELF:</span> ONCE YOU PICK AN ANSWER
+          THERE'S NO GOING BACK, SO THINK CAREFULLY
+        </p> */}
+        {showScore ? (
+          <div className="scoreSection">
+            <p>
+              You scored {score} out of {questions.length}
+            </p>
+            <button onClick={setCurrentQuestion}>Reset Quiz</button>
           </div>
-          <div className="answer">
-            <button>Answer 1</button>
-            <button>Answer 2</button>
-            <button>Answer 3</button>
-            <button>Answer 4</button>
+        ) : (
+          <div className="card">
+            <div className="question">
+              <div className="questionCount">
+                <p>
+                  <span>Question {currentQuestion + 1}</span> /{" "}
+                  {questions.length}
+                </p>
+              </div>
+              <div className="questionText">
+                <p>{questions[currentQuestion].questionText}</p>
+              </div>
+            </div>
+            <div className="answer">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button onClick={() => answerBtnClick(answerOption.isCorrect)}>
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
+            {/* <button onClick={prvQuestion}>Prev</button>
+            <button onClick={nxtQuestion}>Next</button> */}
           </div>
-        </div>
+        )}
       </section>
     </>
   );
